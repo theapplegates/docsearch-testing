c915a48a702d32e3aebd78c0b870e25ae6fbac88
Paul Applegate <paul@theapplegates.com>
2022-10-18 21:17:08 +0000

    ddd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `http://astro.build`,
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    })],
+  site: `http://docsearch.paulapplegate.com`
 });
--- src/config.ts
+++ src/config.ts
@@ -32,13 +32,13 @@ export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
 export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://docsearch.paulapplegate.com`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
-	indexName: 'XXXXXXXXXX',
-	appId: 'XXXXXXXXXX',
-	apiKey: 'XXXXXXXXXX',
+	indexName: 'astro',
+	appId: 'MTKN5SDPYI',
+	apiKey: '9eb9328eaace6e29dc868659d17ff9f9',
 };
 
 export type Sidebar = Record<
